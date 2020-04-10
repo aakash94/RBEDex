2ddaaf494e1e4292c8afe8a7e9e15c98b6f6cd67
Aakash Maroti <aakash.the.limitless@gmail.com>
2020-04-10 13:14:43 +0530

    SACRED SAMPLE
    - Read config properly
    - Log metrics locally

--- Conductor.py
+++ Conductor.py
@@ -1,15 +1,16 @@
 from sacred import Experiment
+from sacred.observers import FileStorageObserver
 from Solver.TestDummy import TestDummy
 
-ex = Experiment()
+ex = Experiment("Test1")
 
 class Conductor:
 
-    def __init__(self, decay_rate=1, n_runs=1, start_val=1):
+    def __init__(self, decay_rate=1, n_runs=1, start_val=1, sacred_ex = None):
 
         # take in all the params and initialize them here
         #self.param = param
-        self.td = TestDummy(rate = decay_rate, n_runs=n_runs, start_val=start_val)
+        self.td = TestDummy(rate = decay_rate, n_runs=n_runs, start_val=start_val, sacred_ex=sacred_ex)
 
     def go_run(self):
 
@@ -24,16 +25,20 @@ def main(decay_rate, n_runs, start_val):
     # actual values will come from command line fom sacred
     param_val = 0  # read this from config or whatever
 
-    cndctr = Conductor(decay_rate=decay_rate, n_runs=n_runs, start_val=start_val)
+    cndctr = Conductor(decay_rate=decay_rate, n_runs=n_runs, start_val=start_val, sacred_ex=ex)
     cndctr.go_run()
 
+
+### Can also use VARIANTS for different agents/experiments
 @ex.config
 def configure():
     # this needs to be a separate method
-    # else main gets called teic
+    # else main gets called twice
     decay_rate = -1
     n_runs = 1
     start_val = 1
+    fso_folder = "Runs"
+    ex.observers.append(FileStorageObserver(fso_folder))
 
 
 if __name__ == '__main__':
--- Solver/TestDummy.py
+++ Solver/TestDummy.py
@@ -1,10 +1,13 @@
+from sacred import Experiment
 
 class TestDummy:
 
-    def __init__(self, rate = -1, n_runs = 1, start_val = 1):
+    def __init__(self, rate = -1, n_runs = 1, start_val = 1, sacred_ex = None):
         self.rate = rate
         self.num_runs = n_runs
         self.start_val = start_val
+        self.run_count = 0
+        self.ex = sacred_ex
         print("TD initialized with \trate=", rate,"\tnum runs=",n_runs,"\tstart val=",start_val )
 
     def decay(self, value, rate):
@@ -15,3 +18,7 @@ class TestDummy:
         for i in range(self.num_runs):
             final_val = self.decay(final_val,self.rate)
             print(i,"\t",final_val)
+
+            # For Sacred Logging
+            self.ex.log_scalar("value" , final_val, self.run_count)
+            self.run_count+=1
